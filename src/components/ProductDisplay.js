0e8e3a5afee3d5ad35c2a0ce8695ad8c7bb93309
Nishino Satoshi <nishino.satoshi.2532@members.co.jp>
2021-03-24 19:56:43 +0900

    develop

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -1,24 +1,21 @@
 app.component('product-display', {
   template:
   /*html*/
-  `<button v-on:click="insertName">{{ name }}</button >`,
+  `<div>{{ name }}</div>`,
   data() {
+    let productName;
+    const url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&shopCode=beams&applicationId=rba_20140612";
+
+    fetch(url).then(response => response.json())
+    .then(json => {
+      productName = json.Items[0].Item.itemName;
+      console.log(productName);
+    });
+
     return {
-      name: "aaa"
+      name: productName
     }
   },
   methods: {
-    insertName() {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&shopCode=beams&applicationId=rba_20140612", true);
-      xhr.responseType = "json";
-
-      xhr.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-          console.log(this.response.Items[0].Item.itemName);
-        }
-      }
-      xhr.send();
-    }
   }
 })
