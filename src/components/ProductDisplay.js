8dd3c3fc5740b2c3bbbb0984211d3bb2ec1b979b
Nishino Satoshi <nishino.satoshi.2532@members.co.jp>
2021-05-07 17:54:10 +0900

    develop

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -3,19 +3,34 @@ app.component('product-display', {
   /*html*/
   `<div>{{ name }}</div>`,
   data() {
-    let productName;
-    const url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&shopCode=beams&applicationId=rba_20140612";
+    const getName = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data.Items[0].Item.itemName;
+    }
 
-    fetch(url).then(response => response.json())
-    .then(json => {
-      productName = json.Items[0].Item.itemName;
-      console.log(productName);
+    const url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&shopCode=beams&applicationId=rba_20140612";
+    getName(url)
+    .then(data => {
+      console.log(data);
     });
 
     return {
-      name: productName
     }
   },
   methods: {
+  },
+  computed: {
+    // name: () => {
+    //   const url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&shopCode=beams&applicationId=rba_20140612";
+    //   const arr = [];
+    //   return fetch(url).then(response => response.json())
+    //   .then(json => {
+    //     json.Items.forEach(item => {
+    //       arr.push(item.Item.itemName);
+    //     });
+    //     return arr;
+    //   });
+    // }
   }
 })
